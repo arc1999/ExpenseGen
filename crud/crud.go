bd2a1dff3fafaeada2d91bfe3b44163f30383d06
ayush <ayushrajchauhan99@gmail.com>
2019-07-03 17:49:09 +0530

    Basic Template Defined for CRUD oprtns

---
+++ crud/crud.go
@@ -0,0 +1,31 @@
+package ExpenseGen
+
+import(
+"net/http"
+)
+func Create(writer http.ResponseWriter , request *http.Request){
+
+//TODO implementation
+
+}
+func Update(writer http.ResponseWriter , request *http.Request){
+
+//TODO implementation
+
+}
+func Delete(writer http.ResponseWriter , request *http.Request){
+
+//TODO implementation
+
+}
+func GetbyId(writer http.ResponseWriter , request *http.Request){
+
+//TODO implementation
+
+}
+func GetAll(writer http.ResponseWriter , request *http.Request){
+
+//TODO implementation
+
+}
+
