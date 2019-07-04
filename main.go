10b1a92c0cc2bdba2d158907910d8132040465f5
shrikar007 <shrikar.vaitala@perennialsys.com>
2019-07-04 12:15:02 +0530

    Request handler created

--- main.go
+++ main.go
@@ -9,45 +9,31 @@ import (
 	"github.com/gobuffalo/packr/v2"
 )
 type Json struct{
-	Packagename string
-	Opname string
-	Opname1 string
-	Opname2 string
-	Opname3 string
-	Opname4 string
+	Struct string
+
 
 }
 // go:generate main -package="ExpenseGen" -Opname="Create" -Opname1="Update" -Opname2="Delete" -Opname3="GetbyId" -Opname4="GetAll"
 func main() {
-	var pkgname,Opertname,Opertname1 ,Opertname2 ,Opertname3,Opertname4  string
-	flag.StringVar(&pkgname,"package","","name of the package")
-	flag.StringVar(&Opertname,"Opname","","name of the operation")
-	flag.StringVar(&Opertname1,"Opname1","","name of the operation")
-	flag.StringVar(&Opertname2,"Opname2","","name of the operation")
-	flag.StringVar(&Opertname3,"Opname3","","name of the operation")
-	flag.StringVar(&Opertname4,"Opname4","","name of the operation")
+	var struc string
+
+	flag.StringVar(&struc,"Structurename","","name of the structure")
 	flag.Parse()
 	box:= packr.New("temp","./templates")
-	t,err:=box.FindString("crud-op.gotpl")
+	t,err:=box.FindString("request-create.gotpl")
 	if err != nil {
 		log.Fatal(err)
 	}
-	tpl,err:= template.New("expensegen").Parse(t)
+	tpl,err:= template.New("request").Parse(t)
 	if err != nil {
 		fmt.Println(err)
 	}
 	data :=Json{
-		Packagename: pkgname,
-		Opname: 	Opertname,
-		Opname1: 	Opertname1,
-		Opname2: 	Opertname2,
-		Opname3: 	Opertname3,
-		Opname4: 	Opertname4,
+		Struct: struc,
 	}
-	file,err := os.Create("./crud/crud.go")
+	file,err := os.Create("./request/request.go")
 	err= tpl.Execute(file,data)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("shrikar")
 }
---
+++ types/structs.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	"time"
+)
+
+type Expense struct {
+	Id          int       `json:"id"`
+	Description	string    `json:"description"`
+	Type        string    `json:"type"`
+	Amount      float64   `json:"amount"`
+	CreatedOn   time.Time `json:"created_on" `
+	UpdatedOn   time.Time `json:"updated_on"`
+}
+
